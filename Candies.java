5f0032d72466a03322e9b485a54ef1eff2ab1497
Karthik Bangera <adikar08@gmail.com>
2020-05-28 19:13:20 -0400

    Completed Candies question

---
+++ Candies.java
@@ -0,0 +1,31 @@
+// Time Complexity :O(n)
+// Space Complexity :O(n)
+// Did this code successfully run on Leetcode :Yes
+// Any problem you faced while coding this :
+
+
+// Your code here along with comments explaining your approach
+
+class Solution {
+    public int candy(int[] ratings) {
+        if(ratings == null || ratings.length==0)    return 0;
+        int[] candies = new int[ratings.length];
+        Arrays.fill(candies, 1);
+        for(int i=1; i < ratings.length; i++)
+        {
+            if(ratings[i]>ratings[i-1])
+            {
+                candies[i]=candies[i-1]+1;
+            }
+        }
+        for(int i=ratings.length-2; i >=0; i--)
+        {
+                if(ratings[i]>ratings[i+1])
+                candies[i]=Math.max(candies[i+1]+1,candies[i]);
+        }
+        int sum=0;
+        for(int e:candies)
+            sum+=e;
+        return sum;
+    }
+}
